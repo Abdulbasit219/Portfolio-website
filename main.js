5e276e686615ff77e16c6b064a39302e8ddb16a0
Abdul Basit <abdulbasitbasit076@gmail.com>
2023-09-08 01:10:13 +0500

    In progree

---
+++ main.js
@@ -0,0 +1,17 @@
+const mobile_navbar = document.querySelector('.icon');
+const navbar = document.querySelector('.nav-items');
+const toggle = () => {
+    navbar.classList.toggle('active');
+    mobile_navbar.classList.toggle('active');
+}
+mobile_navbar.addEventListener('click', () => toggle())
+
+
+// const DownloadResume = () => {
+//     const filePath = 'Abdul Basit resume.pdf';
+//     const a = document.createElement('a');
+//     a.href = filePath;
+//     a.download = 'resume.pdf';
+//     a.click();
+// }
+
